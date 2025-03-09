8eb559074b7507c7c6f322d462aa0c954dad46bf
bintang1855 <bintang.a.p1855@gmail.com>
2025-03-09 19:19:52 +0700

    menambahkan implementasi dengan table-driven

---
+++ tpmod04_103022300058/KodePos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmod04_103022300058
+{
+    class KodePos
+    {
+        public enum Kelurahan {Batununggal, Kujangsari, Mengger, Wates, Cijaura, Jatisari, Margasari, Sekejati, Kebonwaru, Maleer, Samoja }
+
+        // Method untuk mendapatkan kode pos berdasarkan kelurahan
+        public static string GetKodePos(Kelurahan kelurahan)
+        {
+            // Table-driven menggunakan array untuk kode pos
+            string[] kodePos = {"40266", "40287","40267","40256", "40287","40286","40286","40286","40272","40274","40273"
+        };
+
+            return kodePos[(int)kelurahan];
+        }
+    }
+}
--- tpmod04_103022300058/Program.cs
+++ tpmod04_103022300058/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tpmod04_103022300058;
 
-namespace tpmod04_103022300058
-{
-    class Program
+
+class Program
     {
-        static void Main(string[] args)
+    static void Main(){
+        Console.WriteLine("Soal 1. Daftar kelurahan");
+        foreach (KodePos.Kelurahan kelurahan in Enum.GetValues(typeof(KodePos.Kelurahan)))
         {
+            string kodePos = KodePos.GetKodePos(kelurahan);
+            Console.WriteLine("- " + kelurahan + ": " + kodePos);
         }
     }
+
 }
+
+   
